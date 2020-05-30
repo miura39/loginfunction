cee3bbe19d186156c4ed378df0b65acfa92b4bdb
miura39 <miurast.miura.y@gmail.com>
2020-05-30 21:29:30 +0900

    user validation

--- src/bean/UserInfoBean.java
+++ src/bean/UserInfoBean.java
@@ -1,5 +1,43 @@
 package bean;
 
+/**
+ *
+ * @author miurayuuichirou
+ *
+ */
+
 public class UserInfoBean {
 
+	private String userID ="";
+	private String userName="";
+	private String password="";
+
+	public void reset() {
+		this.userID="";
+		this.password="";
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
--- src/util/UserValidation.java
+++ src/util/UserValidation.java
@@ -1,5 +1,22 @@
 package util;
 
-public class UserValidation {
+import bean.UserInfoBean;
+import common.ProductException;
 
+public class UserValidation{
+
+	public void idCheck(UserInfoBean uib) throws ProductException {
+
+
+		String id = uib.getUserID();
+
+		if(id.equals("")) {
+			throw new ProductException("1001W",new Exception());
+		}else if(id.length()>5) {
+			throw new ProductException("1002W",new Exception());
+		}else if(id.matches("^[a-zA-Z0-9]*$")) {
+			throw new ProductException("1003W",new Exception());
+		}
+	}
 }
+
