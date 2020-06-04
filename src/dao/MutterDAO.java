d0fc19288d2f27f8625209a4e3f92aa22b65e832
miura39 <miurast.miura.y@gmail.com>
2020-06-04 23:35:03 +0900

    Mutter　fix

--- src/dao/MutterDAO.java
+++ src/dao/MutterDAO.java
@@ -1,5 +1,16 @@
 package dao;
 
 public class MutterDAO {
+	//データベース接続に使用する情報
+	//注）権限を与えること。GRANT ALL PRIVILEGES ON　〜〜〜〜
+	//注）JDBCのビルドパスを設定すること。
+	private final String DRIVER = "com.mysql.cj.jdbc.Driver";
+//	private final String URL = "jdbc:mysql://localhost:3306/loginaccounts?useSSL=false";
+	private final String URL = "jdbc:mysql://localhost:3306/loginaccounts";
+	private final String USER = "user1";
+	private final String PASS = "password";
+
+
+
 
 }
