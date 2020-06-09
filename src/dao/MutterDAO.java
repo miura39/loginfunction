5eca9e1b3557be78d1f706a597abbb3962159af4
miura39 <miurast.miura.y@gmail.com>
2020-06-09 22:16:49 +0900

    mutterDAO

--- src/dao/MutterDAO.java
+++ src/dao/MutterDAO.java
@@ -1,12 +1,12 @@
 package dao;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
-import java.sql.Connection;
 
 import model.Mutter;
 
@@ -22,8 +22,8 @@ public class MutterDAO {
 	private final String PASS = "password";
 
 	public List<Mutter> findAll(){
-		List<Mutter> mutterList = new ArrayList<>();
 
+		List<Mutter> mutterList = new ArrayList<>();
 
 		try{
 			//データベースへ接続ß
@@ -31,19 +31,27 @@ public class MutterDAO {
 			Connection conn = DriverManager.getConnection(URL,USER,PASS);
 
 			//SELECT文を準備
-			String sql = "SELECT USER_ID,PASS,MAIL,NAME,AGE FROM ACCOUNT WHERE USER_ID = ? AND PASS = ?";
+			String sql = "SELECT id,userName,text FROM MUtter ORDER BY id DESC";
 			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, login.getUserId());
-			pstmt.setString(2, login.getPass());
+
 
 			//SELECT文を実行し、結果を取得
 			ResultSet rs = pstmt.executeQuery();
 
 			if(rs.next()){
-			String userId = rs.getString(columnIndex);
-			String pass = rs.getString("pass");
+			int id = rs.getInt("ID");
+			String userName = rs.getString("NAME");
+			String text = rs.getString("TEXT");
+
+			Mutter mutter = new Mutter(id,userName,text);
+			mutterList.add(mutter);
 
 			}
-		}
+
+	}catch (SQLException | ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+		return mutterList;
+
 	}
 }
